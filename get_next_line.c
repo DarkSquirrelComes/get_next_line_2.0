6cebae24c6274176563c4eff882739328a83a773
Humperdinck Eurybia <heurybia@ga-a3.21-school.ru>
2019-11-20 16:31:33 +0300

    yey

--- get_next_line.c
+++ get_next_line.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: heurybia <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/20 16:18:29 by heurybia          #+#    #+#             */
+/*   Updated: 2019/11/20 16:18:36 by heurybia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 #include <stdlib.h>
 #include <stdio.h>
@@ -6,116 +18,50 @@
 #include <errno.h>
 #include <string.h>
 #include "libft/libft.h"
-/*
-static size_t	strlen(const char *str)
-{
-	size_t		res;
-
-	res = 0;
-	while (*(str++))
-		++res;
-	return (res);
-}
-
-static char		*strcpy(char *dest, const char *src)
-{
-	int	i;
-
-	i = 0;
-	while (src[i] != '\0')
-	{
-		dest[i] = src[i];
-		++i;
-	}
-	dest[i] = src[i];
-	return (dest);
-}
-*/
-char			*strjoin(char const *s1, char const *s2)
-{
-	size_t	l1;
-	size_t	l2;
-	char	*dest;
-
-	if (s1 == 0 || s2 == 0)
-		return (0);
-	l1 = strlen(s1);
-	l2 = strlen(s2);
-	if (!(dest = malloc((l1 + l2 + 1) * sizeof(char))))
-		return (0);
-	strcpy(dest, s1);
-	strcpy(dest + l1, s2);
-	dest[l1 + l2] = 0;
-	return (dest);
-}
-
-
-
-
-/************************
-*	OLD MAMMOTH'S SHIT!	*
-*						*
-*		UP THERE!		*
-*		SERIOUSLY.		*
-*						*
-*************************/
 
 void			concat(char **overhead, char *buf, int *status)
 {
-	char 		*new_oh;
+	char		*new_oh;
 
-	new_oh = strjoin(*overhead, buf);
-    free(buf);
-    free(*overhead);
+	new_oh = ft_strjoin(*overhead, buf);
+	free(*overhead);
 	if (!(new_oh))
 	{
-    	*status = -1;
-    	return ;
+		*status = -1;
+		return ;
 	}
-    *overhead = new_oh;
+	*overhead = new_oh;
 }
 
 void			extend_overhead(char **overhead, int fd, int *status)
 {
-	char		*buf;
-    size_t		len;
+	char		buf[BUFF_SIZE + 1];
+	int			len;
 
-    *status = 1;
-    if (!(buf = (char*)malloc(BUFF_SIZE + 1)))
-    {
-    	*status = -1;
-    	return ;
-    }
-    if ((len = read(fd, (void *)buf, BUFF_SIZE)))
-    {
-        /*if (errno != 0) //cant reach
-        {
-            free(buf);
-            *status = -1;
-        }*/
-        buf[len] = '\0';
-    }
-    else
-        *status = 0;
-    if (*status == 0 || *status == -1)
-    	return ;
-    concat(overhead, buf, status);
+	*status = 1;
+	if ((len = read(fd, (void *)buf, BUFF_SIZE)) > 0)
+		buf[len] = '\0';
+	else
+		*status = len;
+	if (*status == 0 || *status == -1)
+		return ;
+	concat(overhead, buf, status);
 }
 
 int				process_overhead(char **overhead, char **line)
 {
 	char		*new_oh;
-	size_t		len;
+	int			len;
 
-	len = strchr(*overhead, '\n') - *overhead;
+	len = ft_strchr(*overhead, '\n') - *overhead;
 	if (!(*line = malloc(len + 1)))
 		return (-1);
 	else
 		(*line)[len] = '\0';
-	strncpy(*line, *overhead, len);
+	ft_strncpy(*line, *overhead, len);
 	if (*line == 0)
 		return (-1);
-	new_oh = strdup(*overhead + len + 1);
+	new_oh = ft_strdup(*overhead + len + 1);
 	free(*overhead);
 	*overhead = new_oh;
 	return (1);
@@ -123,35 +69,28 @@ int				process_overhead(char **overhead, char **line)
 
 void			str_no_endl(char **overhead, char **line)
 {
-	*line = strdup(*overhead);
+	*line = ft_strdup(*overhead);
 	free(*overhead);
-	*overhead = NULL; //should be empty
+	*overhead = NULL;
 }
 
 int				get_next_line(const int fd, char **line)
 {
-	static char	*overhead = NULL; //NULL
+	static char	*overhead = NULL;
 	int			status;
 
+	if (fd < 0 || line == NULL)
+		return (-1);
 	status = 1;
-	//ft_strnew()
-	if (overhead == 0)
-	{
-		if (!(overhead = malloc(1)))
+	if (overhead == NULL)
+		if (!(overhead = ft_strnew(1)))
 			return (-1);
-		else
-			overhead[0] = '\0';
-	}
-	if (strchr(overhead, '\n'))
-	{
-		//return process
-		status = process_overhead(&overhead, line);
-		return status;
-	}
+	if (ft_strchr(overhead, '\n'))
+		return (process_overhead(&overhead, line));
 	extend_overhead(&overhead, fd, &status);
-	if (status == 0 && strlen(overhead) != 0)
+	if (status == 0 && ft_strlen(overhead) != 0)
 	{
-		str_no_endl(&overhead, line); //status
+		str_no_endl(&overhead, line);
 		return (1);
 	}
 	if (status == -1 || status == 0)
--- get_next_line.h
+++ get_next_line.h
@@ -1,24 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: heurybia <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/20 16:18:46 by heurybia          #+#    #+#             */
+/*   Updated: 2019/11/20 16:18:53 by heurybia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # include <stdlib.h>
 # include <stdio.h>
 # include <unistd.h>
-# include <fcntl.h> 
-# include <errno.h> 
+# include <fcntl.h>
+# include <errno.h>
 
-# define BUFF_SIZE 20
+# define BUFF_SIZE 999
 
-char	*ft_strjoin(char const *s1, char const *s2);
-/************************
-*	OLD MAMMOTH'S SHIT!	*
-*						*
-*		UP THERE!		*
-*		SERIOUSLY.		*
-*						*
-*************************/
 int		get_next_line(const int fd, char **line);
-//additional functions...
-void	concat(char **overhead, char *buf, int *status);
-void	extend_overhead(char **overhead, int fd, int *status);
-int		process_overhead(char **overhead, char **line);
 #endif
